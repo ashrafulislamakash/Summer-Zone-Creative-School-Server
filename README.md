2cf64759d8d00d008e28254c91e4dbe5b57adfb6
ashrafulislamakash <ashrafulislam.eng@gmail.com>
2023-07-29 19:55:03 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+#Summer Zone Creative School#
+
+1 . Clone the Repository: First, you need to clone the GitHub repository containing the React project to your local machine. Use the following command in your terminal or Git Bash:
+
+```javascript
+git clone https://github.com/ashrafulislamakash/Summer-Zone-Creative-School-server.git
+```
+
+2. Navigate to the Project Directory: After cloning the repository, navigate to the project directory using the cd command in the terminal:
+
+```javascript
+cd Summer Zone Creative School
+```
+
+3 Install Dependencies: Once you are in the project directory, install the required dependencies using npm or yarn. In this case, we'll use npm:
+
+```javascript
+npm install
+```
+
+4 Run the Project: After the dependencies are installed, you can start the development server using the following command:
+
+```javascript
+npm run dev
+```
+
+This command will start the Vite development server, and your React project will be running locally at a default URL like http://localhost:3000/
+
+live Site :
+https://summerzone-de0a5.web.app/
---
+++ index.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+// Middleware
+
+app.use(cors());
+app.use(express.json());
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri =
+  "mongodb+srv://summerzone:RUaaANkDOSUHKDex@cluster0.jejh4tn.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const usersCollection = client.db("summerZoneDb").collection("user");
+    const classesCollection = client.db("summerZoneDb").collection("classes");
+    const instructorsCollection = client
+      .db("summerZoneDb")
+      .collection("instructors");
+    const reviewsCollection = client.db("summerZoneDb").collection("reviews");
+    const cartCollection = client.db("summerZoneDb").collection("carts");
+
+    //user collection
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "User Already exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/classes", async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/instructors", async (req, res) => {
+      const result = await instructorsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // cart Collections
+
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const item = req.body;
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Summer Zone Creative School Server Is Running");
+});
+
+app.listen(port, () => {
+  console.log(`Summer Zone Creative School Server Is Running ${port}`);
+});
